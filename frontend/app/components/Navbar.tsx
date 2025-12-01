2abd56167b69291b04e9dc8fa42a043ce0827487
Netero <m_benab@live.concordia.ca>
2025-12-01 18:49:04 -0500

    Create Navbar component and modify layout file

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <nav className="fixed top-0 w-full items-center">
+            <p>Hi there</p>
+        </nav>
+    )
+}
+
+export default Navbar
