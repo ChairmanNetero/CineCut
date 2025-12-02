35858741a04cae78d1546a7faf39907f7196e36b
Netero <m_benab@live.concordia.ca>
2025-12-01 19:59:58 -0500

    Create a Navbar without logo and styling

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,11 +1,36 @@
-import React from "react"
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <nav className="fixed top-0 w-full items-center">
-            <p>Hi there</p>
-        </nav>
-    )
-}
+  return (
+    <nav
+      className="fixed top-0 w-full flex items-center 
+    justify-around py-5 px-24 border-b border-gray-700 bg-black"
+    >
 
-export default Navbar
+      <Link href="/">
+      <p>logo placeholder</p></Link>
+
+      <ul className="flex gap-10 text-lg">
+        <Link href="/" className="text-gray-300 hover:text-white transition-colors">
+        Home
+        </Link>
+      </ul>
+
+      <ul className="flex gap-10 text-lg">
+        <Link href="/watchlist" className="text-gray-300 hover:text-white transition-colors">
+        Watchlist
+        </Link>
+      </ul>
+
+      <ul className="flex gap-10 text-lg">
+        <Link href="/history" className="text-gray-300 hover:text-white transition-colors">
+        History
+        </Link>
+      </ul>
+
+    </nav>
+  );
+};
+
+export default Navbar;
